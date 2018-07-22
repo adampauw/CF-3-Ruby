c72bf54c494fee07f1f9d401ba336acc0ae52a0c
adam pauw <adampauw@gmail.com>
2018-07-22 17:21:06 +0200

    Show computers hand

--- RPS.rb
+++ RPS.rb
@@ -6,28 +6,32 @@ human_score = 0
 until human_score == 3 || comp_score ==3
     p "Choose rock paper or scissors"
 
-human = gets.chomp.downcase
-computer = ["rock", "paper", "scissors"].sample
+    human = gets.chomp.downcase
+    computer = ["rock", "paper", "scissors"].sample
 
 
 if
-    (human == "rock" && computer == "scissors") || (human == "scissors" && computer == "paper") || (human == "paper" && computer == "scissors")
-    p "You are the winner"
+    (human == "rock" && computer == "scissors") || (human == "scissors" && computer == "paper") || (human == "paper" && computer == "rock")
+    p "You are the winner!"
     human_score += 1
 
 elsif
     (human == "rock" && computer == "rock") || (human == "scissors" && computer == "scissors") || (human == "paper" && computer == "paper")
     p "That theres what we call a draw"
-    
+
 elsif
     human != ("rock" || "paper" || "scissors")
     p "Your options are: Rock, Paper or Scissors"
 
 else
-    p "Computer wins"
+    p "Computer wins!"
     comp_score += 1
 end
 
-p "Human Score: #{human_score}"
-p "Computer Score: #{comp_score}"
+    p "Computer chose: #{computer}"
+
+    p "Human Score: #{human_score}"
+    p "Computer Score: #{comp_score}"
+
+
 end

b4928c8fdeea06d0b0d9262da1d991a9d9d1e0e3
adam pauw <adampauw@gmail.com>
2018-07-22 14:24:25 +0200

    Add scoring system

--- RPS.rb
+++ RPS.rb
@@ -1,21 +1,33 @@
 puts "Let's play Rock, Paper, Scissors!"
 
+comp_score = 0
+human_score = 0
+
+until human_score == 3 || comp_score ==3
+    p "Choose rock paper or scissors"
+
 human = gets.chomp.downcase
 computer = ["rock", "paper", "scissors"].sample
+
 
 if
     (human == "rock" && computer == "scissors") || (human == "scissors" && computer == "paper") || (human == "paper" && computer == "scissors")
     p "You are the winner"
+    human_score += 1
 
 elsif
     (human == "rock" && computer == "rock") || (human == "scissors" && computer == "scissors") || (human == "paper" && computer == "paper")
     p "That theres what we call a draw"
+    
 elsif
     human != ("rock" || "paper" || "scissors")
     p "Your options are: Rock, Paper or Scissors"
-elsif
-    human = exit
-    exit
+
 else
     p "Computer wins"
+    comp_score += 1
+end
+
+p "Human Score: #{human_score}"
+p "Computer Score: #{comp_score}"
 end
